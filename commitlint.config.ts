adebebc87f134fc2d511d8c496d4f5a2eb4ede4e
Victor Zhang <victorzw895@gmail.com>
2024-06-05 17:07:43 +1000

    feat(setup): fix commit rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,8 +1,8 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
-		'subject-empty': [2, 'always'],
-		'body-empty': [2, 'never'],
+		'subject-empty': [2, 'never'],
+		// 'body-empty': [2, 'always'], // disable prompt
 		'footer-empty': [2, 'always'],
 	},
 };
