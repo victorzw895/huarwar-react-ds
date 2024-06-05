e21ded0e1ed5ffae015f9a79f058eda1c0ada402
Victor Zhang <victorzw895@gmail.com>
2024-06-05 16:52:49 +1000

    feat(setup): commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': [2, 'never'],
+	},
+	skipQuestions: ['body', 'footer'],
+};
