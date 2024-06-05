9c96072e93c12d02ebd62a47727e67e1e04fdcfc
Victor Zhang <victorzw895@gmail.com>
2024-06-05 16:30:17 +1000

    first commit

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,6 @@
+export const breakpoints = {
+	mobile: 0,
+	tablet: 768,
+	desktop: 1024,
+	largeDesktop: 1440,
+};
---
+++ src/styles/colors.ts
@@ -0,0 +1,121 @@
+export const palette = {
+	backgroundPeaGreen30: '#EDF0E1',
+	backgroundPeaGreen70: '#F4F6ED',
+	backgroundPeaGreen100: '#F8F9F3',
+	backgroundEnchantedGreen30: '#F4F6F4',
+	backgroundEnchantedGreen70: '#ECF0EC',
+	backgroundEnchantedGreen100: '#E0E9E0',
+	backgroundMysteriousGreen30: '#F4FAF8',
+	backgroundMysteriousGreen70: '#EEF7F4',
+	backgroundMysteriousGreen100: '#E3F2ED',
+	backgroundOptimisticBlue30: '#F7FCFD',
+	backgroundOptimisticBlue70: '#F0F9FA',
+	backgroundOptimisticBlue100: '#EAF7F9',
+	backgroundSaltyTear30: '#F0F6F7',
+	backgroundSaltyTear70: '#E4F1F3',
+	backgroundSaltyTear100: '#D8E9EB',
+	backgroundSeasickGreen30: '#F8F9F3',
+	backgroundSeasickGreen70: '#F4F0DA',
+	backgroundSeasickGreen100: '#EEE8C8',
+	backgroundChiplipYellow30: '#FCFBEF',
+	backgroundChiplipYellow70: '#F9F8E2',
+	backgroundChiplipYellow100: '#F5F4D0',
+	backgroundTepidTea30: '#FFFBF4',
+	backgroundTepidTea70: '#FEF7EC',
+	backgroundTepidTea100: '#FEF2E0',
+	backgroundLikeableSand30: '#FCF9F5',
+	backgroundLikeableSand70: '#FAF4EE',
+	backgroundLikeableSand100: '#F6EDE2',
+	backgroundPracticalBeige30: '#F8F1EF',
+	backgroundPracticalBeige70: '#F6E8E5',
+	backgroundPracticalBeige100: '#EFE3DD',
+	backgroundGreyExpectations30: '#F7F7F7',
+	backgroundGreyExpectations70: '#F1F0F0',
+	backgroundGreyExpectations100: '#E7E6E6',
+	backgroundFooter: '#4F555A',
+	backgroundCalculator: '#1F1F1F',
+	lightThemeSuccess: '#1B68B5',
+};
+
+export const themeColors = {
+	//primary
+	primaryLighter: '#F7F7F7',
+	primaryLight: '#818181',
+	primaryDefault: '#2069B3',
+	primaryDark: '#545454',
+	primaryDarker: '#000000',
+
+	//secondary
+	secondary1: '#D7DFB9',
+	secondary2: '#C1D1C8',
+	secondary3: '#CAE4DD',
+	secondary4: '#CCEAF0',
+	secondary5: '#B6D3DD',
+	secondary6: '#E9E1B9',
+	secondary7: '#F1F0BD',
+	secondary8: '#F7E6CC',
+	secondary9: '#F8E1D4',
+	secondary10: '#EFD8E3',
+	secondary11: '#B7BEB6',
+
+	// accent
+	accent1: '#00C389',
+	accent2: '#F99FC9',
+	accent3: '#0072CE',
+	accent4: '#F3D03E',
+	accent5: '#EF3340',
+	accent6: '#FF5B35',
+
+	// neutral
+	neutralWhite: '#FFFFFF',
+	neutralLighter: '#F6F6F6',
+	neutral300: '#E7E6E6',
+	neutralLight: '#E7E6E6',
+	neutralMid: '#9E9E9E',
+	neutralDark: '#666666',
+	neutralDarker: '#000000',
+
+	// system
+	systemSuccessDark: '#008661',
+	systemSuccessLight: '#EBF3EC',
+	systemErrorDark: '#BA0025',
+	systemErrorLight: '#FFE9EB',
+	systemWarningDark: '#F5CE3E',
+	systemWarningLight: '#F8ECE2',
+	systemInfoDark: '#2069B3',
+	systemInfoLight: '#D5E9EB',
+
+	// interaction
+	interactionDefault: '#2069B3',
+	interactionHover: '#0F4E8D',
+	interactionFocus: '#0F4E8D',
+	interactionFocusStroke: '#2069B380',
+	interactionDisabledFill: '#E7E6E6',
+	interactionDisabledText: '#9E9E9E',
+
+	// form
+	formDefault: '#6F6F6F',
+
+	// text
+	textHeaderDark: '#000000',
+	textHeaderMid: '#555555',
+	textHeaderLight: '#FFFFFF',
+	textBodyDark: '#000000',
+	textBodyMid: '#555555',
+	textBodyLight: '#FFFFFF',
+	textAccentDark: '#19252E',
+	textAccentLight: '#E8EEF2',
+	textDisabled: '#767676',
+
+	// tag
+	tagDark: '#000000',
+	tagLight: '#E7E6E6',
+
+	// background
+	backgroundOptimisticBlue: '#E0F2F6',
+
+	// header banners
+	greyExpectationsLight: '#E6E9E6',
+	saltyTearLight: '#D3E5EB',
+	practicalBeigeLight: '#F5E8EE',
+};
